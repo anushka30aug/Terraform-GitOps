e6afafa040e2975fc26a4fe0c71b7682d27cd6c3
Anushka Shukla <anushkashukla3003@gmail.com>
2026-04-13 15:05:59 +0530

    zap:add subnets

--- main.tf
+++ main.tf
@@ -17,3 +17,14 @@ resource "aws_vpc" "main" {
     Name = "main-vpc-ap-southeast-2"
   }
 }
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+}
